828b19047a9c65944eb868216895d90793956706
Zhong Liu <laxtiz@live.com>
2022-05-18 01:05:47 +0800

    just a demo for linechart

---
+++ scripts/post_extra_script.py
@@ -0,0 +1,13 @@
+Import("env", "projenv")
+
+print("env", "=" * 40)
+print(env)
+
+print("env.Dump()", "=" * 40)
+print(env.Dump())
+
+print("projenv", "=" * 40)
+print(projenv)
+
+print("projenv.Dump()", "=" * 40)
+print(projenv.Dump())
---
+++ scripts/pre_extra_script.py
@@ -0,0 +1,7 @@
+Import("env")
+
+print("env", "=" * 40)
+print(env)
+
+print("env.Dump()", "=" * 40)
+print(env.Dump())
